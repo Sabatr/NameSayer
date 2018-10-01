697e889200f2d8b491cdf58ffce6e3ad256327bb
Sabatr <bnguyen12152@gmail.com>
2018-10-01 18:29:49 +1300

    Added a few changes

--- src/main/java/app/tools/FileFinder.java
+++ src/main/java/app/tools/FileFinder.java
@@ -11,17 +11,26 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
+/**
+ * This class allows the user to find files in their system.
+ */
 public class FileFinder {
     private String _type;
     private File _chosenFile;
     public FileFinder(String type) {
         _type = type;
     }
+
+    /**
+     * Chooses whether to open a directory chooser or txt file chooser
+     * @param stage
+     */
     public void choose(Stage stage) {
         if (_type.equals("sound")) {
             DirectoryChooser directoryChooser = new DirectoryChooser();
             directoryChooser.setTitle("Directory chooser");
             _chosenFile = directoryChooser.showDialog(stage);
+            //NOTE: nothing else is done with the directory system yet.
         } else if (_type.equals("practice")) {
             FileChooser fileChooser = new FileChooser();
             FileChooser.ExtensionFilter extensionFilter = new FileChooser.ExtensionFilter("TEXT files (*.txt)","*.txt");
@@ -31,6 +40,10 @@ public class FileFinder {
         }
     }
 
+    /**
+     *
+     * @return the content in a txt file. Currently, it separates the spaces and hyphens.
+     */
     public ObservableList<NameEntry> getContent() {
         ObservableList<NameEntry> list = FXCollections.observableArrayList();
         if (_chosenFile != null) {
